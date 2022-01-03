95f569078137d7d2ba1655e2ff2be5dc522ddfa1
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2022-01-03 21:22:37 +0700

    tugas hari pertama selesai

--- 1-1-Tugas-OOP-PHP/index.php
+++ 1-1-Tugas-OOP-PHP/index.php
@@ -14,34 +14,100 @@ trait Fight
 {
   public $attackPower, $defencePower;
 
-  abstract public function serang();
-  abstract public function diserang();
+  abstract public function serang($defencePower);
+  abstract public function diserang($defencePower);
 }
 
 class Elang extends Hewan
 {
   use Fight;
 
-
-  public function serang()
+  public function __construct($jumlahKaki = 2, $keahlian = "terbang tinggi", $attackPower = 10, $defencePower = 5, $nama = "Elang")
   {
-    return 'a';
+    $this->jumlahKaki = $jumlahKaki;
+    $this->keahlian = $keahlian;
+    $this->attackPower = $attackPower;
+    $this->defencePower = $defencePower;
+    $this->nama = $nama;
   }
-  public function diserang()
+
+  public function serang($defencePower)
   {
-    return 'x';
+    echo PHP_EOL;
+    echo "$this->nama sedang menyerang harimau";
+    echo PHP_EOL;
+    $this->diserang($defencePower);
+  }
+  public function diserang($defencePower)
+  {
+    $this->darah = $this->darah - $this->attackPower / $defencePower;
+    echo "harimau sedang diserang darah sisa $this->darah";
   }
 
   function atraksi()
   {
-    return 'a';
+
+    return "$this->nama sedang $this->keahlian";
   }
-  public function getInfoHewan($nama)
+
+  public function getInfoHewan($jenis)
   {
-    return $this->nama = $nama;
+    return "Info Hewan" . PHP_EOL .
+      "jumlah kaki " . $this->jumlahKaki . PHP_EOL .
+      "keahlian " . $this->keahlian . PHP_EOL .
+      "kekuatan serang " . $this->attackPower . PHP_EOL .
+      "pertahanan " . $this->defencePower . PHP_EOL .
+      "jenis binatang " . $jenis;
+  }
+}
+class Harimau extends Hewan
+{
+  use Fight;
+
+  public function __construct($jumlahKaki = 4, $keahlian = "lari cepat", $attackPower = 7, $defencePower = 8, $nama = "Harimau")
+  {
+    $this->jumlahKaki = $jumlahKaki;
+    $this->keahlian = $keahlian;
+    $this->attackPower = $attackPower;
+    $this->defencePower = $defencePower;
+    $this->nama = $nama;
+  }
+
+  public function serang($defencePower)
+  {
+    echo PHP_EOL;
+    echo "$this->nama sedang menyerang Elang";
+    echo PHP_EOL;
+    $this->diserang($defencePower);
+  }
+  public function diserang($defencePower)
+  {
+    $this->darah = $this->darah - $this->attackPower / $defencePower;
+    echo "Elang sedang diserang darah sisa $this->darah";
+  }
+
+  function atraksi()
+  {
+
+    return "$this->nama sedang $this->keahlian";
+  }
+
+  public function getInfoHewan($jenis)
+  {
+    return "Info Hewan" . PHP_EOL .
+      "jumlah kaki " . $this->jumlahKaki . PHP_EOL .
+      "keahlian " . $this->keahlian . PHP_EOL .
+      "kekuatan serang " . $this->attackPower . PHP_EOL .
+      "pertahanan " . $this->defencePower . PHP_EOL .
+      "jenis binatang " . $jenis;
   }
 }
 
-$tes = new Elang;
 
-echo $tes->getInfoHewan('Harimau');
+$elang = new Elang();
+echo $elang->getInfoHewan(get_class($elang));
+$elang->serang(10);
+
+$harimau = new Harimau();
+echo $harimau->getInfoHewan(get_class($harimau));
+$harimau->serang(10);
