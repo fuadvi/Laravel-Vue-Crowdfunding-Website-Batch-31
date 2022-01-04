a806b05f25e6f06fa17b8899687239286f82967a
Teuku Fuad Maulana <fuad.pcd@gmail.com>
2022-01-04 17:29:44 +0700

    buat migration dan model role

---
+++ crowdfunding-website/app/Models/Role.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Role extends Model
+{
+    protected $fillable = ['nama'];
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (!$model->{$model->getKeyName()}) {
+                $model->{$model->getKeyName()} = Str::uuid();
+            }
+        });
+    }
+}
