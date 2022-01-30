fe3ca3f3827478dec5db7fdcb77d7e24189beda2
bidadarisurga <fuad.pcd@gmail.com>
2022-01-30 10:09:38 +0700

    add vuex persis

--- crowdfunding-website/resources/js/store.js
+++ crowdfunding-website/resources/js/store.js
@@ -1,21 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
 import alert from './module/alert'
 import auth from './module/auth'
 import dialog from './module/dialog'
 import transaction from './module/transaction'
 
+const vuexPersistence = new VuexPersistence({
+    key: "myApp",
+    storage : localStorage
+})
 
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+    plugins: [vuexPersistence.plugin],
     modules: {
         transaction,
         alert,
         auth,
         dialog
-
     }
 
 })
